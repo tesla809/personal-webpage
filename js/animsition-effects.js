de548d1e4a0ebb1b686f3ea8585a34415ec090ec
Anthony Albertorio <anthony.albertorio@gmail.com>
2015-10-14 23:45:54 -0400

    added animsitiion-effects.js for jquery effects to index.html

--- js/animsition-effects.js
+++ js/animsition-effects.js
@@ -1,7 +1,24 @@
 $(".animsition").animsition({
   inClass: 'fade-in-left-sm',
   outClass: 'fade-out-left-sm',
-  linkElement: 'nav header a',
+  linkElement: 'navbar navbar-default a',
   inDuration: 1500,
   outDuration: 500
 });
+
+$(".nav").animsition({
+  inClass: 'fade-in-left-sm',
+  outClass: 'fade-out-left-sm',
+  linkElement: 'navbar navbar-default a',
+  inDuration: 1500,
+  outDuration: 500
+});
+
+$(".jumbotron").animsition({
+  inClass: 'fade-in-right-lg',
+  outClass: 'fade-out-right-lg',
+  linkElement: 'navbar navbar-default a',
+  inDuration: 1500,
+  outDuration: 500
+});
+
