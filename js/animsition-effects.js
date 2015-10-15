85723f1cbda23964fec1541b4883cc0490758bec
Anthony Albertorio <anthony.albertorio@gmail.com>
2015-10-15 14:43:14 -0400

    added rotation to jumbotron, delay of fade in left for nav bar right

--- js/animsition-effects.js
+++ js/animsition-effects.js
@@ -9,7 +9,7 @@ $(".animsition").animsition({
 $(".nav").animsition({
   inClass: 'fade-in-left-sm',
   outClass: 'fade-out-left-sm',
-  linkElement: 'navbar navbar-default a',
+  linkElement: 'animsition-link',
   inDuration: 1500,
   outDuration: 500
 });
@@ -17,6 +17,7 @@ $(".nav").animsition({
 $(".jumbotron").animsition({
   inClass: 'rotate-in-sm',
   outClass: 'rotate-out-sm',
+  linkElement: 'btn',
   inDuration: 1500,
   outDuration: 500
 });
