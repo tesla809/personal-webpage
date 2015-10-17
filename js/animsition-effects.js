a4a0977420610cf6d1a15f280ad7dbad6dc08763
Anthony Albertorio <anthony.albertorio@gmail.com>
2015-10-17 03:44:48 -0400

    added header, need css fix

--- js/animsition-effects.js
+++ js/animsition-effects.js
@@ -6,18 +6,9 @@ $(".animsition").animsition({
   outDuration: 500
 });
 
-$(".nav").animsition({
-  inClass: 'fade-in-left-sm',
-  outClass: 'fade-out-left-sm',
-  linkElement: 'animsition-link',
-  inDuration: 1500,
-  outDuration: 500
-});
-
-$(".jumbotron").animsition({
-  inClass: 'rotate-in-sm',
-  outClass: 'rotate-out-sm',
-  linkElement: 'btn',
+$(".jumbotron animsition").animsition({
+  inClass: 'rotate-in-lg',
+  outClass: 'rotate-out-lg',
   inDuration: 1500,
   outDuration: 500
 });
